e4124268239ae3f0fa33d457ac9a30e354ec256a
Akamshu <39032612+Akamshu@users.noreply.github.com>
2021-04-26 13:35:52 +0100

    Added a Third Box for Recovered and Background

--- src/components/DataBoxes.vue
+++ src/components/DataBoxes.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="grid md:grid-cols-2 gap-4">
     <!-- Box 1 -->
-    <div class="shadow-md bg-blue-100 p-10 text-center rounded">
-      <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+    <div class="shadow-md bg-orange-100 p-10 text-center rounded">
+      <h3 class="text-3xl text-pink-700 font-bold mb-4"><i class="fa fa-bed"></i></h3>
 
       <div class="text-2xl mb-4">
         <span class="font-bold">New:</span>
@@ -15,8 +15,23 @@
     </div>
 
     <!-- Box 2 -->
-    <div class="shadow-md bg-blue-200 p-10 text-center rounded">
-      <h3 class="text-3xl text-blue-900 font-bold mb-4">Deaths</h3>
+    <div class="shadow-md bg-green-300 p-10 text-center rounded">
+      <h3 class="text-3xl text-green-700 font-bold mb-4"><i class="fa fa-running"></i></h3>
+
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Recovered:</span>
+        {{ numberWithCommas(stats.NewRecovered) }}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total:</span>
+        {{ numberWithCommas(stats.TotalRecovered) }}
+      </div>
+    </div>
+
+
+    <!-- Box 3 -->
+    <div class="shadow-md bg-red-400 p-10 text-center rounded">
+      <h3 class="text-3xl text-red-600 font-bold mb-4"><i class="fa fa-cross"></i></h3>
 
       <div class="text-2xl mb-4">
         <span class="font-bold">New:</span>
